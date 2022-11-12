e69cb7c199a4717ed4dc72f0f8505babd7d5f9c0
Melikyan Rafael <melikyanrafael13@gmail.com>
2022-11-12 16:43:44 +0300

    Добавление компонентов

---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="v-main-wrapper">
+        <p>{{title}}</p>
+        <v-catalog />
+        <v-cart />
+    </div>
+</template>
+
+<script>
+    import vCatalog from "./v-catalog.vue";
+    import vCart from "./v-cart.vue";
+    export default {
+        name: 'v-main-wrapper',
+        components: {
+            vCatalog,
+            vCart
+        },
+        props: {},
+        data() {
+            return {
+                title: 'Main wrapper'
+            }
+        },
+        computed: {},
+        methods: {},
+        watch: {},
+        mounted () {
+            console.log('Start mounted')
+        }
+    }
+</script>
+
+<style scoped>
+    .v-main-wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        max-width: 900px;
+        margin: 0 auto;
+    }
+</style>
