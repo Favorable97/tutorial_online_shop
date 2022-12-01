ec43ad1d8528a37f00f2a92a844346a868e605d3
Melikyan Rafael <melikyanrafael13@gmail.com>
2022-12-01 16:18:30 +0300

    Добавление данных
    Была добавлена статическая информация для наших карточек. Рендерили список этих данных. Заполняем данными элементы. Отображаем данные на html странице. Кроме текста, ещё картинки. В :src необходимо прописывать require. Передали данные из потомка к родителю

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -2,7 +2,12 @@
     <div class="v-catalog">
         <h1>Catalog</h1>
         <div class="v-catalog__list">
-            <v-catalog-item/>
+            <v-catalog-item
+                v-for="product in products"
+                :key="product.article"
+                :product_data="product"
+                @sendArticle="showChildArticleToConsole"
+            />
         </div>
     </div>
 </template>
@@ -16,9 +21,64 @@
         },
         props: {},
         data() {
-            return {}
+            return {
+                products: [
+                    {
+                        image: "1.jpg",
+                        name: "T-shirt 1",
+                        price: 100,
+                        article: "T1",
+                        available: true,
+                        category: "Мужские"
+                    },
+                    {
+                        image: "2.jpg",
+                        name: "T-shirt 2",
+                        price: 150,
+                        article: "T2",
+                        available: true,
+                        category: "Женские"
+                    },
+                    {
+                        image: "3.jpg",
+                        name: "T-shirt 3",
+                        price: 200,
+                        article: "T3",
+                        available: false,
+                        category: "Женские"
+                    },
+                    {
+                        image: "4.jpg",
+                        name: "T-shirt 4",
+                        price: 300,
+                        article: "T4",
+                        available: true,
+                        category: "Мужские"
+                    },
+                    {
+                        image: "5.jpg",
+                        name: "T-shirt 5",
+                        price: 500,
+                        article: "T5",
+                        available: false,
+                        category: "Женские"
+                    },
+                    {
+                        image: "6.jpeg",
+                        name: "T-shirt 6",
+                        price: 700,
+                        article: "T6",
+                        available: true,
+                        category: "Женские"
+                    }
+                ]
+            }
         },
-
+        methods: {
+            showChildArticleToConsole(data) {
+                console.log(data)
+            }
+        }
     }
 </script>
 
